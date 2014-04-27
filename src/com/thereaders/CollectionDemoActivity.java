0892c39cba2e37b1a4594666d65239eca2cc257c
likeusmoment <likeusmoment@gmail.com>
2014-04-28 01:34:29 +0900

    ui: add basic swiping views with tabs
    add basic swiping views with tabs
    
    Signed-off-by: likeusmoment <likeusmoment@gmail.com>

---
+++ src/com/thereaders/CollectionDemoActivity.java
@@ -0,0 +1,25 @@
+package com.thereaders;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class CollectionDemoActivity extends FragmentActivity {
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+    ViewPager mViewPager;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_demo);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mDemoCollectionPagerAdapter =
+                new DemoCollectionPagerAdapter(
+                        getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+    }
+}
