8104f74a15269b2f941c947109d2df10b22b7323
likeusmoment <likeusmoment@gmail.com>
2014-05-11 18:01:59 +0900

    DB: add integration class to hold all kinds of database objects in an object
    add integration class to hold all kinds of database objects in an object
    
    Signed-off-by: likeusmoment <likeusmoment@gmail.com>

---
+++ src/com/thereaders/DataCollection.java
@@ -0,0 +1,40 @@
+package com.thereaders;
+
+public class DataCollection {
+	private Author author;
+	private Book book;
+	private Note note;
+	private Publisher publisher;
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public Note getNote() {
+		return note;
+	}
+
+	public void setNote(Note note) {
+		this.note = note;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+}
