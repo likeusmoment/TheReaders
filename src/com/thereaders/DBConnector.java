eff2242b9ab7d1fd8837b2ec03f8c84b7f6005cd
likeusmoment <likeusmoment@gmail.com>
2014-05-13 21:34:52 +0900

    db: modify db connector
    1. bugfix in creating tables
    2. add select all from db about author, book
    3. add the function to insert testcases to DB
    
    Signed-off-by: likeusmoment <likeusmoment@gmail.com>

--- src/com/thereaders/DBConnector.java
+++ src/com/thereaders/DBConnector.java
@@ -1,11 +1,15 @@
 package com.thereaders;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 
 public class DBConnector extends SQLiteOpenHelper {
 
@@ -32,30 +36,26 @@ public class DBConnector extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("create table ? "
-				+ "(idx int unsigned primary key auto_increment,"
-				+ "name varchar(30) unique)",
-				new String[] { TBL_AUTHOR });
+		db.execSQL("create table " + TBL_AUTHOR 
+				+ "(idx integer primary key autoincrement,"
+				+ "name varchar(30) unique)");
 
-		db.execSQL("create table ? "
-				+ "(idx int unsigned primary key auto_increment,"
-				+ "name varchar(30) unique)",
-				new String[] { TBL_PUBLISHER });
+		db.execSQL("create table " + TBL_PUBLISHER 
+				+ "(idx integer primary key autoincrement,"
+				+ "name varchar(30) unique)");
 
-		db.execSQL("create table ? "
-				+ "(idx int unsigned primary key auto_increment,"
-				+ "author_idx int unsigned,"
-				+ "publisher_idx int unsigned,"
+		db.execSQL("create table " + TBL_BOOK
+				+ "(idx integer primary key autoincrement,"
+				+ "author_idx integer,"
+				+ "publisher_idx integer,"
 				+ "name varchar(100) unique,"
-				+ "num_page int unsigned)",
-				new String[] { TBL_BOOK });
+				+ "num_page integer)");
 
-		db.execSQL("create table ? "
-				+ "(book_idx int unsigned,"
+		db.execSQL("create table " + TBL_NOTE
+				+ "(book_idx integer,"
 				+ "txt varchar(400),"
-				+ "num_page int unsigned,"
-				+ "datetime datetime)",
-				new String[] { TBL_NOTE });
+				+ "num_page integer,"
+				+ "datetime datetime)");
 	}
 
 	@Override
@@ -91,5 +91,111 @@ public class DBConnector extends SQLiteOpenHelper {
 
 		return author;
 	}
+
+	public DataCollection[] getAllBooks() {
+		SQLiteDatabase db = this.getReadableDatabase();
+		
+		Cursor cursor = db.query(TBL_BOOK,
+				new String[] { COL_IDX, COL_AUTHOR_IDX, COL_PUBLISHER_IDX, COL_NAME, COL_NUM_PAGE },
+				null, null, null, null, null, null);
+
+		if (cursor != null)
+			cursor.moveToFirst();
+		
+		DataCollection[] dataCollections = new DataCollection[cursor.getCount()];
+		
+		for(int i = 0; i < cursor.getCount(); i++) {
+			dataCollections[i] = new DataCollection(new Book(cursor.getInt(0), cursor.getInt(1),
+					cursor.getInt(2), cursor.getString(3), cursor.getInt(4)));
+			cursor.moveToNext();
+		}
+
+		return dataCollections;
+	}
+	
+	public DataCollection[] getAllAuthor() {
+		SQLiteDatabase db = this.getReadableDatabase();
+		
+		Cursor cursor = db.query(TBL_AUTHOR,
+				new String[] { COL_IDX, COL_NAME },
+				null, null, null, null, null, null);
+
+		if (cursor != null)
+			cursor.moveToFirst();
+		
+		DataCollection[] dataCollections = new DataCollection[cursor.getCount()];
+		
+		for(int i = 0; i < cursor.getCount(); i++) {
+			dataCollections[i] = new DataCollection(new Author(cursor.getInt(0), cursor.getString(1)));
+			cursor.moveToNext();
+		}
+
+		return dataCollections;
+	}
+
+	public void addTestcases() {
+		SQLiteDatabase db = this.getWritableDatabase();
+		long result;
+
+		try {
+			for (int i = 0; i < 10; i++) {
+				ContentValues values = new ContentValues();
+				values.put("name", "author" + i);
+
+				result = db.insert(TBL_AUTHOR, null, values);
+			}
+
+			for (int i = 0; i < 10; i++) {
+				ContentValues values = new ContentValues();
+				values.put("name", "publisher" + i);
+
+				result = db.insert(TBL_PUBLISHER, null, values);
+			}
+
+			for (int i = 0; i < 10; i++) {
+				ContentValues values = new ContentValues();
+				values.put("author_idx", i);
+				values.put("publisher_idx", i);
+				values.put("name", "book" + i);
+				values.put("num_page", i * 100);
+
+				result = db.insert(TBL_BOOK, null, values);
+			}
+
+			for (int i = 0; i < 10; i++) {
+				ContentValues values = new ContentValues();
+				values.put("book_idx", i);
+				values.put("txt", "txt" + i);
+				values.put("num_page", i * 100);
+				SimpleDateFormat dateFormat = new SimpleDateFormat(
+						"yyyy-MM-dd HH:mm:ss");
+				Date date = new Date();
+				values.put("datetime", dateFormat.format(date));
+
+				result = db.insert(TBL_NOTE, null, values);
+			}
+		} catch (Exception e) {
+		}
+	}
+	
+//	public DataCollection[] getNotes(int idx) {
+//		SQLiteDatabase db = this.getReadableDatabase();
+//		
+//		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+//		qb.setTables(TBL_NOTE );
+//
+//		Cursor cursor = db.query(TBL_NOTE,
+//				new String[] { COL_IDX, COL_NAME },
+//				COL_IDX + "=?",
+//				new String[] { String.valueOf(idx) },
+//				null, null, null, null);
+//
+//		if (cursor != null)
+//			cursor.moveToFirst();
+//
+//		Author author = new Author(cursor.getInt(0), cursor.getString(1));
+//
+//		return author;
+//	}
 
 }
