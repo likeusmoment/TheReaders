e6dfb2eba259e68d3dd7a806bfbbc6349839c1c1
likeusmoment <likeusmoment@gmail.com>
2014-05-13 22:28:31 +0900

    JSON: modify the json parser to work
    modify the json parser to work
    
    Signed-off-by: likeusmoment <likeusmoment@gmail.com>

--- src/com/thereaders/JSONParser.java
+++ src/com/thereaders/JSONParser.java
@@ -1,17 +1,5 @@
 package com.thereaders;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,102 +7,19 @@ import android.util.Log;
 
 public class JSONParser {
 
-    static InputStream is = null;
-    static JSONObject jObj = null;
-    static String json = "";
-    
-    // url to make request
-    private static String url = "http://api.9android.net/contacts";
+	public JSONObject parseJson() {
+		JSONObject jObj = null;
+		String json = "{ \"glossary\": { \"title\": \"example glossary\"}}";
 
-    // JSON Node names
-    private static final String TAG_CONTACTS = "contacts";
-    private static final String TAG_ID = "id";
-    private static final String TAG_NAME = "name";
-    private static final String TAG_EMAIL = "email";
-    private static final String TAG_ADDRESS = "address";
-    private static final String TAG_GENDER = "gender";
-    private static final String TAG_PHONE = "phone";
-    private static final String TAG_PHONE_MOBILE = "mobile";
-    private static final String TAG_PHONE_HOME = "home";
-    private static final String TAG_PHONE_OFFICE = "office";
+		try {
+			jObj = new JSONObject(json);
 
-        
-    // constructor
-    public JSONParser() {}
+			JSONObject c = jObj.getJSONObject("glossary");
+			Log.d("hwh", "hwh:" + c.getString("title"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-    public JSONObject getJSONFromUrl() {
-
-        JSONObject json = jParser.getJSONFromUrl(url);
-
-        try {
-            // Getting Array of Contacts
-            JSONArray contacts = json.getJSONArray(TAG_CONTACTS);
-
-            // looping through All Contacts
-            for(int i = 0; i < contacts.length(); i++){
-                JSONObject c = contacts.getJSONObject(i);
-
-                // Storing each json item in variable
-                String id = c.getString(TAG_ID);
-                String name = c.getString(TAG_NAME);
-                String email = c.getString(TAG_EMAIL);
-                String address = c.getString(TAG_ADDRESS);
-                String gender = c.getString(TAG_GENDER);
-
-                // Phone number is agin JSON Object
-                JSONObject phone = c.getJSONObject(TAG_PHONE);
-                String mobile = phone.getString(TAG_PHONE_MOBILE);
-                String home = phone.getString(TAG_PHONE_HOME);
-                String office = phone.getString(TAG_PHONE_OFFICE);
-
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-    	
-        // Making HTTP request
-        try {
-            // defaultHttpClient
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
-
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            is = httpEntity.getContent();
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            is.close();
-            this.json = sb.toString();
-        } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
-        }
-
-        // try parse the string to a JSON object
-        try {
-            jObj = new JSONObject(this.json);
-        } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
-        }
-
-        // return JSON String
-        return jObj;
-        
-    }
+		return jObj;
+	}
 }
-
